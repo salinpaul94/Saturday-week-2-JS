03392720f093118ec3d69a0d785526e9265ffb6a
salinpaul94 <salinpv@hotmail.com>
2022-10-03 21:37:10 +0530

    Arithamatic operation

---
+++ Addition.js
@@ -0,0 +1,44 @@
+function take_input() {
+    // get values from html tags using id
+    var ops1 = document.getElementById('op1').value;
+    var ops2 = document.getElementById('op2').value;
+    var operators = document.getElementById('operator').value; // get value from options
+
+    ops1 = Number.parseInt(ops1); // convert values to integer
+    ops2 = Number.parseInt(ops2);
+    var res;
+    switch (operators) {
+        case '+' :
+            res = ops1 + ops2;
+            break;
+        case '-' :
+            res = ops1 - ops2;
+            break;
+        case '*' :
+            res = ops1 * ops2;
+            break;
+        case '/' :
+            res = ops1 / ops2;
+            break;
+    }
+    // print the result in <p> tag with id result1
+    document.getElementById("result1").innerHTML = res;
+}
+
+function eval_input() {
+    try {
+        // expression is evaluated in eval(document.getElementById('exp').value)
+        // the result is stored in expression variable
+        var expression = eval(document.getElementById('exp').value);
+
+        // check if the result is valid or not
+        if(expression == "Infinity" || expression == "Undefined"){
+            throw new Error('Incorrect input!')
+        } else {
+            // print the result in <p> tag with id result2
+            document.getElementById('result2').innerHTML = expression;
+        } 
+    } catch(err) {
+        document.getElementById('result2').innerHTML = err;
+    }
+}
